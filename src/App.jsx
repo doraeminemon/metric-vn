c863661a2f03577276f4edb97dd3bc047cbd8735
Tyson Do <doraeminemon@gmail.com>
2024-05-08 13:24:28 +0700

    Move breadcrumb into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 // import { useState } from 'react'
 import './App.css'
 import { Header } from './components/Header'
+import { Breadcrumb } from './components/Breadcrumb'
 
 function App() {
-
   return (
     <>
       <div className='relative w-full bg-white md:py-4'>
         <Header />
+        <Breadcrumb />
         <div className='footer'>
         </div>
       </div>
---
+++ src/components/Breadcrumb.jsx
@@ -0,0 +1,28 @@
+export function Breadcrumb() {
+  const path = [
+      'Máy tính & Laptop',
+      <svg key="right-caret" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="flex-shrink-0 h-5 w-5 text-gray-600"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>,
+      'Phụ Kiện Máy Tính',
+      <svg key="right-caret" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="flex-shrink-0 h-5 w-5 text-gray-600"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>,
+      'Bộ chia cổng USB & Đọc thẻ nhớ',
+  ]
+  return (
+    <div id="breadcrumb" className='flex flex-row items-center px-8 py-2 gap-2 border-b border-b-gray-300'>
+      <div className='home icon cursor-pointer'>
+        <a href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 h-5 w-5">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+            </path>
+          </svg>
+        </a>
+      </div>
+      {path.map((param) => (
+        <span key={param} className='cursor-pointer'>
+          <a href="/">
+            {param}
+          </a>
+        </span>
+      ))}
+    </div>
+  )
+}
