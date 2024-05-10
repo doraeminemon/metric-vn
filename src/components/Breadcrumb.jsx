8fd38cfd02e6d4afb8aeb5c476ee6b83c745aa83
Tyson Do <doraeminemon@gmail.com>
2024-05-10 09:20:07 +0700

    Grouping so that Breadcrumbs and Header to have limited width

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -7,22 +7,24 @@ export function Breadcrumb() {
       'Bộ chia cổng USB & Đọc thẻ nhớ',
   ]
   return (
-    <div id="breadcrumb" className='flex flex-row items-center px-8 py-2 gap-2 border-b border-b-gray-300'>
-      <div className='home icon cursor-pointer'>
-        <a href="/">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 h-5 w-5">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
-            </path>
-          </svg>
-        </a>
-      </div>
-      {path.map((param) => (
-        <span key={param} className='cursor-pointer'>
+    <div id="breadcrumb" className='flex flex-row items-center justify-center px-8 py-2 border-b border-b-gray-300'>
+      <div className="max-w-[1240px] flex flex-row items-center min-w-[1240px] px-4 gap-2">
+        <div className='home icon cursor-pointer'>
           <a href="/">
-            {param}
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 h-5 w-5">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+              </path>
+            </svg>
           </a>
-        </span>
-      ))}
+        </div>
+        {path.map((param) => (
+          <span key={param} className='cursor-pointer'>
+            <a href="/">
+              {param}
+            </a>
+          </span>
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,28 @@
 export function Header() {
   return (
-    <div id="header" className='header flex items-center border-b border-b-gray-300 pb-4 px-4'>
-      <div className='header bg-white py-3 px-4 flex flex-shrink-0 cursor-pointer'>
-        <a href="/" className='text-orange-600 font-bold hover:text-orange-600 text-xl'>MuTôMi</a>
-      </div>
-      <div className='flex-grow w-full relative'>
-        <div className='relative'>
-          <input className='placeholder-gray-400 w-full border border-gray-300 bg-white h-10 px-5 pr-12 rounded-full text-base focus:outline-none text-gray-800' placeholder='Dán link hoặc tìm sản phẩm'></input>
+    <div id="header" className='header flex border-b border-b-gray-300 pb-4 px-4 justify-center'>
+      <div className="items-center flex max-w-[1240px] w-full">
+        <div className=' bg-white py-3 px-4 flex flex-shrink-0 cursor-pointer'>
+          <a href="/" className='text-orange-600 font-bold hover:text-orange-600 text-xl'>MuTôMi</a>
         </div>
-        <div className="absolute min-w-12 aspect-h1-w1 h-full inset-y-0 right-0 flex items-center justify-center cursor-pointer rounded-r-full text-primary-500 hover:text-blue-500 text-orange-600">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-          </svg>
+        <div className='flex-grow w-full relative'>
+          <div className='relative'>
+            <input className='placeholder-gray-400 w-full border border-gray-300 bg-white h-10 px-5 pr-12 rounded-full text-base focus:outline-none text-gray-800' placeholder='Dán link hoặc tìm sản phẩm'></input>
           </div>
+          <div className="absolute min-w-12 aspect-h1-w1 h-full inset-y-0 right-0 flex items-center justify-center cursor-pointer rounded-r-full text-primary-500 hover:text-blue-500 text-orange-600">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+            </svg>
+            </div>
+        </div>
+        <div className='flex flex-shrink-0 ml-4 flex-row items-center cursor-pointer'>
+          <svg width="18" height="13" viewBox="0 0 18 13" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className=""><rect data-v-4a0f219a="" width="18" height="1" transform="matrix(1 0 0 -1 0 1)" fill="black"></rect><rect data-v-4a0f219a="" width="18" height="1" transform="matrix(1 0 0 -1 0 7)" fill="black"></rect><rect data-v-4a0f219a="" width="18" height="1" transform="matrix(1 0 0 -1 0 13)" fill="black"></rect></svg>
+          <span className='ml-2 font-semibold'>DANH MỤC</span>
+        </div>
+        <a className='relative mx-4 font-semibold' href="/blog">
+          BLOG
+        </a>
       </div>
-      <div className='flex flex-shrink-0 ml-4 flex-row items-center cursor-pointer'>
-        <svg width="18" height="13" viewBox="0 0 18 13" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className=""><rect data-v-4a0f219a="" width="18" height="1" transform="matrix(1 0 0 -1 0 1)" fill="black"></rect><rect data-v-4a0f219a="" width="18" height="1" transform="matrix(1 0 0 -1 0 7)" fill="black"></rect><rect data-v-4a0f219a="" width="18" height="1" transform="matrix(1 0 0 -1 0 13)" fill="black"></rect></svg>
-        <span className='ml-2 font-semibold'>DANH MỤC</span>
-      </div>
-      <a className='relative mx-4 font-semibold' href="/blog">
-        BLOG
-      </a>
     </div>
   )
 }
